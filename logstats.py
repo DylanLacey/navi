45297f18ad2803ae0234fb8cf1755f017f390a80
dylanlacey <github@dylanlacey.com>
2017-03-30 17:24:27 +1000

    Serve tracking script

---
+++ logstats.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import time, threading
+import subprocess
+import re
+import httplib, urllib
+
+def send_memory_to_server():
+	params = create_post_params(check_memory())
+	connection = httplib.HTTPSConnection('exposition-fairy.herokuapp.com')
+	request = connection.request('POST', '/stat/memory', params)
+	print(connection.getresponse().read())
+
+def create_post_params(memory):
+	t = time.ctime()
+	active = memory["active"]
+	free = memory["free"]
+	total = memory["total"]
+
+	return urllib.urlencode({'active': active, 'free': free, 'total': total, 'time': t})
+
+def log_memory_repeatedly():
+	send_memory_to_server()
+	threading.Timer(10, log_memory_repeatedly).start()
+
+def check_memory():
+	ps = subprocess.Popen(['ps', '-caxm', '-orss,comm'], stdout=subprocess.PIPE).communicate()[0].decode()
+	vm = subprocess.Popen(['vm_stat'], stdout=subprocess.PIPE).communicate()[0].decode()
+	
+	# Iterate processes
+	processLines = ps.split('\n')
+	sep = re.compile('[\s]+')
+	rssTotal = 0 # kB
+	for row in range(1,len(processLines)):
+	    rowText = processLines[row].strip()
+	    rowElements = sep.split(rowText)
+	    try:
+	        rss = float(rowElements[0]) * 1024
+	    except:
+	        rss = 0 # ignore...
+	    rssTotal += rss
+
+	# Process vm_stat
+	vmLines = vm.split('\n')
+	sep = re.compile(':[\s]+')
+	vmStats = {}
+	for row in range(1,len(vmLines)-2):
+	    rowText = vmLines[row].strip()
+	    rowElements = sep.split(rowText)
+	    vmStats[(rowElements[0])] = int(rowElements[1].strip('\.')) * 4096
+
+	return {'active': (vmStats["Pages active"]/1024/1024), 'free': (vmStats["Pages free"]/1024/1024), 'total': ( rssTotal/1024/1024 )}
+	#('Active:\t\t%d MB, Free:\t\t%d MB, Total:\t%.3f MB' % ((vmStats["Pages active"]/1024/1024),(vmStats["Pages free"]/1024/1024),( rssTotal/1024/1024 )))
+
+log_memory_repeatedly()
+#log_memory_repeatedly()
--- routing.rb
+++ routing.rb
@@ -8,6 +8,11 @@ get '/' do
   'Navi is a tiny stat tracker of no real utility.'
 end
 
+get '/memory_script.py' do
+  location = File.join Dir.pwd, 'logstats.py'
+  return [200, File.read(location)]
+end
+
 post '/stat/:name' do |name|
   #logger.debug "Logging #{n}"
   tracker.log params['time'], 'active', params['active']
